343daa7a2a71b448bca6f02f1ea9848fea80d4ac
Edward Krupicka <87044013+edwardkrupicka@users.noreply.github.com>
2021-12-15 10:50:46 -0700

    Create basic form

---
+++ src/Components/Form/Form.js
@@ -0,0 +1,15 @@
+import './Form.css'
+
+const Form = () => {
+
+	
+	return (
+		<form>
+			<h2>Form Goes Here</h2>
+			<input type='text' ></input>
+			<button>+</button>
+		</form>
+	)
+}
+
+export default Form;
